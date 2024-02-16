ea8b7ef3b3b8c4470da692762e607e832b8f3959
Akhil Shukla <akhil@binder-sa.com>
2024-02-16 15:12:58 +0530

    Three sum

--- Program.cs
+++ Program.cs
@@ -9,17 +9,7 @@ namespace DSA.Program
         public static void Main(string[] args)
         {
 
-            int[][] arr = new int[3][]
-                {
-                    new int[] { 0,1,2,0 }, 
-                    new int[] { 3,4,5,2 }, 
-                    new int[] { 1,3,1,5 } 
-                };
-            SetZeroesMatrix.SetZeroes(arr);
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine("Row " + i + ": " + String.Join(", ", arr[i]));
-            }
+            
         }
     }
 }
---
+++ Questions/ThreeSum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Questions
+{
+    public class ThreeSum
+    {
+        public static IList<IList<int>> TwoSumMethod(int[] nums, int sum)
+        {
+            IList<IList<int>> res = new List<IList<int>>(); 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == sum)
+                    {
+                        List<int> temp = new();
+                        temp.Add(nums[i]);
+                        temp.Add(nums[j]);
+                        res.Add(temp);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public static IList<IList<int>> ThreeSumMethod(int[] nums)
+        {
+
+            IList<IList<int>> finalRes = new List<IList<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0 - nums[i];
+                var res = TwoSumMethod(nums, sum);
+                if (res != null && res.Any())
+                {
+                    foreach (var list in res)
+                    {
+                        list.Add(nums[i]);
+                        finalRes.Add(list);
+                    }
+                        
+
+                   
+                        
+                }
+            }
+            return finalRes;
+        }
+    }
+}
