4a7e511dd4c3ab352692cfb79c12aaefdabb6b7a
Akhil Shukla <akhil@binder-sa.com>
2024-02-19 15:02:15 +0530

    Larget subarray with zero sum

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ namespace DSA.Program
         public static void Main(string[] args)
         {
 
-            
+            int[] arr = new[] {6, -2, 2, -8, 1, 7, 4, -10};
+            int[] arr2 = new[] {1, 0, -5};
+            var res  = LargestSubarrayWithZeroSum.GetLargestSubarrayWithZeroSum(arr);
+            Console.WriteLine(res);
         }
     }
 }
---
+++ Questions/LargestSubarrayWithZeroSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Questions
+{
+    public class LargestSubarrayWithZeroSum
+    {
+        public static int GetLargestSubarrayWithZeroSum(int[] arr)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int maxi = Int32.MinValue;
+            int sum = 0;
+            for(int i = 0; i< arr.Length; i++) 
+            {
+               sum += arr[i];
+               if(sum == 0)
+               {
+                    maxi = i > maxi ? i + 1 : maxi;
+               }
+               else
+               {
+                    if(map.ContainsKey(sum))
+                    {
+                        maxi = Math.Max(maxi, i - map[sum]);
+                    }
+                    else
+                    {
+                        map.Add(sum, i);
+                    }
+               }
+            }
+
+            return maxi;
+        }
+    }
+}
